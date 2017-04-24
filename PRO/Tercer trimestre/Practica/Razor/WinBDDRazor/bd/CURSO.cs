c156b13bac097022d906de2c22f8439f5c2662cd
Emiliano Montesdeoca del Puerto <emontesdeoc@gmail.com>
2017-04-24 10:43:20 +0100

    Razor

---
+++ PRO/Tercer trimestre/Practica/Razor/WinBDDRazor/bd/CURSO.cs
@@ -0,0 +1,37 @@
+namespace WinBDDRazor
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("CURSOS")]
+    public partial class CURSO
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public CURSO()
+        {
+            ALUMNOS = new HashSet<ALUMNO>();
+            NOTAS = new HashSet<NOTA>();
+        }
+
+        [Key]
+        [StringLength(10)]
+        public string COD_CUR { get; set; }
+
+        [StringLength(30)]
+        public string DESCRIPCION { get; set; }
+
+        public int? HORAS { get; set; }
+
+        [StringLength(30)]
+        public string TUTOR { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<ALUMNO> ALUMNOS { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<NOTA> NOTAS { get; set; }
+    }
+}
---
+++ PRO/Tercer trimestre/Practica/Razor/WinBDDRazor/logic/cursos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WinBDDRazor
+{
+
+    public class cursos
+    {
+
+        public static string hola { get; set; } = "valor de clase";
+    }
+}
