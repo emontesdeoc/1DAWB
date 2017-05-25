2b72543f39afadcacb085801c92ff4aa2e9fec43
Emi <emontesdeoc@gmail.com>
2017-05-25 13:45:48 +0100

    BD

--- BD/Tercer trimestre/Examen11052017.sql
+++ BD/Tercer trimestre/Examen11052017.sql
@@ -1,6 +1,9 @@
 -- Emiliano Montesdeoca del Puerto 
+
+
 --1.- Procedimiento almacenado al que le pasemos como parámetro el nombre de un Distrito y devuelva en un parámetro de salida la suma 
 --del Area de los barrios de ese distrito o un -1 si el distrito no existe. 
+
 USE datossctfe 
 go 
 
@@ -57,94 +60,101 @@ go
 --un select válido dividido en dos campos (uno con el select y los campos y otro desde el from) y una tabla de destino. 
 --Se trata de que el programa borre la tabla de destino si existe y mediante la sentencia select la cree y cargue de datos. 
 
---if OBJECT_ID('tablauxiliar') is not null  drop table TablaIndices; 
---go 
---create table tablaauxiliar (tabladestino VARCHAR(100), seleccion1 VARCHAR(max), seleccion2 VARCHAR(max)) 
---go 
-
-IF object_id('tablaaxuiliar') IS NOT NULL DROP table tablaaxuiliar
-create table tablaaxuiliar ( tabladestino varchar(100),  seleccion1 varchar(max),  seleccion2 varchar(max))
-go 
-
-CREATE PROCEDURE sp_ejer3
-as
-
-DECLARE @tablaorigen TABLE
-(
-  tabladestino varchar(100),  seleccion1 varchar(max),  seleccion2 varchar(max)
-)
-
-INSERT INTO @tablaorigen (tabladestino,seleccion1,seleccion2)
-  SELECT tabladestino, seleccion1,seleccion2
-    FROM tablaaxuiliar
-
-declare @sentencia1 nvarchar(max);
-declare @sentencia2 nvarchar(max);
-declare @tabladestino nvarchar(max);
-declare @seleccion1 nvarchar(max);
-declare @seleccion2 nvarchar(max);
-
-declare CUR cursor for select  tabladestino, seleccion1,seleccion2 from  @tablaorigen; 
-open CUR 
-fetch next from CUR into  @tabladestino, @seleccion1, @seleccion2
-
--- por cada campo en tablaauxiliar
-while @@FETCH_STATUS=0
-begin	
-
--- pillar el nombre de la tabla
-declare @test nvarchar(max) =  @seleccion2;
-declare @test2 nvarchar(max);
-set @test2 = (SELECT SUBSTRING(@test,6,100) as Value);
-set @test2 = SUBSTRING(@test2,0,CHARINDEX(' ', @test2));
-print @test2;
-
--- buscar los nombres de los campos y su tipo
-
-declare CUR2 cursor for select  COLUMN_NAME,DATA_TYPE from  information_schema.columns  where table_name = @test2; 
-open CUR2 
-fetch next from CUR2 into  
-
-while @@FETCH_STATUS=0
-begin	
-set @sentencia1 = 'select COLUMN_NAME,DATA_TPYE '+@seleccion2
-set @sentencia2 = 'Create table ' + @tabladestino + '( barrio,)';
-
-
-end
-
-
-set @sentencia2 = 'Create table ' + @tabladestino + '( barrio,)';
-
-
-end
- 
-select * from information_schema.columns where table_name = 'tablaaxuiliar'
-
 --4. Función de tabla en línea que nos muestre la suma del valor de datospadron por barrio para grupos de edad que contengan una 
 --edad pasada como parámetro. Hacer un ejemplo de uso. 
 
+CREATE FUNCTION F_datospadron (@eNumero INT) 
+returns TABLE 
+AS 
+    RETURN 
+      (SELECT Sum(datospadron.valor) AS Suma, 
+              grupoedad.edadinicial  AS Inicial, 
+              grupoedad.edadfinal    AS Final 
+       FROM   datospadron 
+              INNER JOIN barrio 
+                      ON barrio.cod_barrio = datospadron.cod_barrio 
+              INNER JOIN grupoedad 
+                      ON datospadron.clavegrupo = grupoedad.clave 
+       WHERE  @eNumero BETWEEN grupoedad.edadinicial AND grupoedad.edadfinal 
+       GROUP  BY grupoedad.edadfinal, 
+                 grupoedad.edadinicial); 
+
+go 
+
+SELECT suma, 
+       inicial, 
+       final 
+FROM   dbo.F_datospadron(12); 
+
+go 
 
 --5.- Función de tabla mediante instrucciones que le pasemos como parámetro una letra (A,X,Y) 
 --Para cada barrio dará el nombre y el valor de:  
 --  Para la A dar Area  
 --  Para la X dar UTM_X  
 --  Para la Y dar UTM_Y  
+go
+
+CREATE FUNCTION dbo.F_barrio (@letra VARCHAR(1)) 
+returns @newtable TABLE ( 
+  barrio   NVARCHAR(max), 
+  variable FLOAT ) 
+AS 
+  BEGIN 
+      IF @letra = 'A' 
+        INSERT @newtable 
+        SELECT barrio, 
+               area 
+        FROM   barrio 
+      ELSE IF @letra = 'X' 
+        INSERT @newtable 
+        SELECT barrio, 
+               utm_x 
+        FROM   barrio 
+      ELSE IF @letra = 'Y' 
+        INSERT @newtable 
+        SELECT barrio, 
+               utm_y 
+        FROM   barrio 
+
+      RETURN 
+  END; 
+
+go 
+
+SELECT barrio, 
+       variable 
+FROM   F_barrio('A') 
+
+go 
 
 
 --6- Crear un índice para la tabla Paradas_Taxi por el campo direccion.  
 
+CREATE INDEX ix_paradastaxi 
+  ON paradas_taxi(direccion) 
+
+go 
 
 --7.- Crear una vista que nos muestre edadinicial y edad final de cada grupoedad. Hacer un ejemplo de uso.
 
+CREATE VIEW v_veredades AS SELECT DISTINCT(grupoedad.edadinicial), grupoedad.edadfinal FROM grupoedad SELECT edadinicial, edadfinal FROM v_veredades 
 
 --8.- Recuperar copia de seguridad indicando objetos que contiene del tipo indicado en la lista: 
+
 --  Tablas de usuario y nº de registros  
 --  Índices  
 --  Foreign key  
 --  Funciones de usuario  
 --  Procedimientos almacenados de usuario  
 
+-- Tiene 3 tablas
+--		Familia -> tiene 6 registros || Tiene 3 indices
+--		paso -> no tiene registros || Tiene 1 indice
+--		Planta -> tiene 17 registrs || Tiene 1 indice
+-- Tiene 1 procedimiento almacenado
+-- Tiene 1 funciona
+-- Tiene 
 
 --9.- Crear la tabla paso con la estructura siguiente:  
 --  fechahora datetime  
@@ -152,3 +162,41 @@ select * from information_schema.columns where table_name = 'tablaaxuiliar'
 --  usuario varchar(200)  
 --Añadir un trigger a la tabla distrito que cuando hagamos un alta de distrito añada un registro a la tabla logdistrito 
 --con el coddistrito añadido, el usuario obtenido de SYSTEM_USER y la fechahora de getdate().  
+
+CREATE TABLE paso 
+  ( 
+     fechahora   DATETIME, 
+     coddistrito INT, 
+     usuario     VARCHAR(200) 
+  ) 
+
+CREATE TABLE logdistrito 
+  ( 
+     fechahora   DATETIME, 
+     coddistrito INT, 
+     usuario     VARCHAR(200) 
+  ) 
+
+go 
+
+CREATE TRIGGER t_paso 
+ON paso 
+after INSERT 
+AS 
+    DECLARE @a INT 
+
+    SELECT @a = inserted.coddistrito 
+    FROM   inserted 
+
+    INSERT INTO logdistrito 
+    VALUES     (Getdate(), 
+                @a, 
+                SYSTEM_USER) 
+
+    INSERT INTO paso 
+    VALUES     ('12/02/1995', 
+                12345, 
+                'emi') 
+
+    SELECT * 
+    FROM   logdistrito 
